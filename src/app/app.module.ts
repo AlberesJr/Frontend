5b0642557debd9e22cdaf4208d9c0c0e3087a2a1
OxentechDevTeam <desenvolvimento@oxentech.com.br>
2019-06-12 00:36:33 -0300

    criando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,30 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+import { ROUTES } from './app.routes'
+
 import { AppComponent } from './app.component'
 import { AuthService } from 'services/auth.service';
 import { StorageService } from 'services/storage.service';
+import { HeaderComponent } from 'app/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [
     AuthService,
